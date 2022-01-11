8f84278a3282eb231f3f6aefea0007bd253f861e
Chloe Decker <69217587+TheOnlyMrLucifer@users.noreply.github.com>
2022-01-11 15:29:32 +0100

    V1
    V1

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/TheOnlyMrLucifer/LuciferV9Bot /LuciferV9Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /LuciferV9Bot
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
